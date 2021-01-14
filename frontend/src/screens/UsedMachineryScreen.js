5545c3d0260e1bb016c2e944209ecd82dd37f68e
Ryan Haire <RWHaire@hotmail.com>
2021-01-13 20:50:43 -0500

    added product component and jumbtron to used and new machinery pages

--- frontend/src/screens/UsedMachineryScreen.js
+++ frontend/src/screens/UsedMachineryScreen.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Jumbotron } from 'react-bootstrap'
 import Footer from '../components/Footer'
 import Navibar from '../components/Navibar'
 
@@ -7,6 +8,12 @@ const UsedMachineryScreen = () => {
         <>
             <header>
                 <Navibar/>
+                <Jumbotron className="jumbotron">
+                    <h1>New Machinery</h1>
+                    <p  style={{width: '26rem'}} className="fs-md">
+                        Looking for a lower cost? Explore our collection of used machinery.
+                    </p>
+                </Jumbotron>
             </header>
             <main>
 
